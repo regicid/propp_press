b634adb55d3251c1f9c3b56ae63736f7e7a5d559
Benoit De Courson <b.de-courson@LSE-Mac-b.de-courson-G4VQDR7RV6>
2026-05-20 17:31:45 +0200

    changes

--- propp_fr/src/propp_fr/propp_fr_coreference_resolution_module.py
+++ propp_fr/src/propp_fr/propp_fr_coreference_resolution_module.py
@@ -132,8 +132,8 @@ def get_mentions_embeddings(entities_df, tokens_embeddings_tensor):
     if len(entities_df) == 0:
         return torch.empty(0, device=tokens_embeddings_tensor.device)
 
-    start_tokens = entities_df['start_token'].to_numpy(copy=True)
-    end_tokens = entities_df['end_token'].to_numpy(copy=True)
+    start_tokens = entities_df['start_token'].astype(np.int64).to_numpy(copy=True)
+    end_tokens = entities_df['end_token'].astype(np.int64).to_numpy(copy=True)
 
     start_embeddings = tokens_embeddings_tensor[start_tokens]
     end_embeddings = tokens_embeddings_tensor[end_tokens]
